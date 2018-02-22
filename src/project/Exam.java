665a10e8ee1741f109756541de8f0ee221107df9
skyleobrien <skyleobrien@C218-1749.yccc.edu>
2018-02-21 19:25:54 -0500

    extra comments

--- src/project/Exam.java
+++ src/project/Exam.java
@@ -16,19 +16,19 @@ public class Exam {
 		int rightCount = 0;
 		int finalScore = 0;
 		try {
-			BufferedReader qFile = new BufferedReader(new FileReader("src/project/Question"));
-			BufferedReader aFile = new BufferedReader(new FileReader("src/project/Answer"));
+			BufferedReader qFile = new BufferedReader(new FileReader("src/project/Question")); ///starts to read question file
+			BufferedReader aFile = new BufferedReader(new FileReader("src/project/Answer")); ///starts to read answer file
 
 			List<Question> qList = new ArrayList<Question>();
 			String start = " ";
-			while (start != null) {
+			while (start != null) { /// creates a start string which receives the values of each line in the question file
 
 				start = qFile.readLine();
-				if (start != null) {
+				if (start != null) { /// without this I was getting an issue where I had an extra empty question being passed
 					Question q = new Question(start); /// reads in each question
 					qList.add(q);
 				}
-				
+
 			}
 			for (Question q : qList) {
 				q.setAnswer(aFile.readLine()); /// gives each question an answer
@@ -38,7 +38,7 @@ public class Exam {
 					System.out.println(q.getQuestion());
 
 				String response = console.next();
-				while (!response.equals("t") && !response.equals("f")) {
+				while (!response.equals("t") && !response.equals("f")) { /// handles potential errors resulting from improper input
 					System.out.println("Please answer each question t or f");
 					response = console.next();
 				}
@@ -50,10 +50,10 @@ public class Exam {
 				}
 
 			}
-			finalScore = rightCount * 10;
+			finalScore = rightCount * 10; /// gives complete exam score
 			System.out.println("You scored " + finalScore + " points");
 			BufferedWriter score = null;
-			FileWriter testScore = new FileWriter("src/project/score", true);
+			FileWriter testScore = new FileWriter("src/project/score", true); ///initializes file writer
 			score = new BufferedWriter(testScore);
 			score.write("You scored " + finalScore + " points \n");
 			score.close();
